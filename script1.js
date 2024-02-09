1d57376d6e9d09e57f1965a2e4b4ad4de40a785a
srinija landa <srinijalanda@gmail.com>
2024-02-10 00:03:47 +0530

    edit part left

--- script1.js
+++ script1.js
@@ -22,17 +22,16 @@ addEventListener("submit", (eventobj) => {
     EmailId: eventobj.target.Eemail.value,
     company: eventobj.target.ecompany.value,
   };
- // Reset the form
- eventobj.target.reset();
+  // Reset the form
+  eventobj.target.reset();
   /**
-   * let pass the employeeObj in the function 
+   * let pass the employeeObj in the function
    * which is responsole for creating tr and td
    */
 
   addEmployee(employeeObj);
- 
+  toggleForm();
 });
-
 
 /**
  * Access the tbody Tag
@@ -45,24 +44,57 @@ addEventListener("submit", (eventobj) => {
  * but object excluded with id,edit column we need add Manually
  */
 
-const tbodyTag=document.getElementById("tbody");
-let employeId=234;
-function addEmployee(obj){
-    const trTag=document.createElement('tr'); //<tr></tr>
-    const tdId=document.createElement('td');
-    tdId.innerText=employeId;
-    employeId++;
-    trTag.appendChild(tdId)
-    //let run the loop for the object using for in
-    for(let key in obj){
-      //  console.log(obj[key]);
-      const tdTag=document.createElement('td');
-      tdTag.innerText=obj[key];
-      trTag.appendChild(tdTag);
-    }
-    //last tdedit
-    const tdedit=document.createElement('td');
-    tdedit.innerHTML=`<button>delete</button> <button>Edit</button>`;
-    trTag.appendChild(tdedit);
-    tbodyTag.appendChild(trTag)
+const tbodyTag = document.getElementById("tbody");
+let employeId = 234;
+function addEmployee(obj) {
+  const trTag = document.createElement("tr"); //<tr></tr>
+  trTag.id=employeId;
+  const tdId = document.createElement("td");
+  tdId.innerText = employeId;
+  employeId++;
+  trTag.appendChild(tdId);
+  //let run the loop for the object using for in
+  for (let key in obj) {
+    //  console.log(obj[key]);
+    const tdTag = document.createElement("td");
+    tdTag.innerText = obj[key];
+    trTag.appendChild(tdTag);
+  }
+  //last tdedit
+  /**
+   * let create delete and edit button
+   *  <td><button>Delete</button><button>Edit</button></td>
+   * append to tr
+   * manually add the eventListener to delete and edit
+   */
+  const tdedit = document.createElement("td");
+  const deletebutton = document.createElement("button");
+  deletebutton.innerText = "Delete";
+  const editbutton = document.createElement("button");
+  editbutton.innerText = "Edit";
+  tdedit.append(deletebutton, editbutton);
+  // tdedit.innerHTML=`<button>delete</button> <button>Edit</button>`;
+  trTag.appendChild(tdedit);
+  tbodyTag.appendChild(trTag);
+  /**
+   * add the eventListener
+   */
+  deletebutton.addEventListener("click",deleteButton); //passfunctionref
+  editbutton.addEventListener("click",editButton); //passfunctionref
+}
+
+function deleteButton(event) {
+  //event Listener callback for delete button
+  console.log(event.target); //button.parentNode=><td>.parentNode=><tr>
+  //using parentNode property
+  let buttontag=event.target;
+ let tdTag= buttontag.parentNode;
+ let trTag=tdTag.parentNode;
+ trTag.remove();
+
+}
+
+function editButton(event) {
+  //event Listener call back for the dit button
+  console.log(event.target);
 }
